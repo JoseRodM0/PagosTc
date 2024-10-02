cac0a3bf1d0398b4f48a0bd238e87de2e592850e
JoseRodM0 <majorod1203@gmail.com>
2024-10-02 07:57:23 -0700

    Agregar archivos de proyecto.

---
+++ PagosTc/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PagosTc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string codigo = DateTime.Now.ToString("yymmss");
+            Console.WriteLine(codigo);
+
+            Console.WriteLine("Introduce tu codigo de verificacion: ");
+            string codigoEscrito = Console.WriteLine();
+
+            if (codigoEscrito == codigo)
+                [
+                    Console.WriteLine("ok");
+
+            ]
+              
+
+
+        }
+    }
+}
